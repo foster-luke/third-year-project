becf06d217e5cda826611262518f25b19801fb90
Luke Foster <foster.luke@hotmail.com>
2022-02-15 20:26:46 +0000

    Add tensorFlow example to React code

--- src/components/TensorFlow/TensorFlow.jsx
+++ src/components/TensorFlow/TensorFlow.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import * as tf from '@tensorflow/tfjs';
 
 class TensorFlow extends React.Component {
     constructor(props) {
@@ -9,8 +10,21 @@ class TensorFlow extends React.Component {
     }
 
     trainModel() {
-        console.log(this);
-        this.setState({modelTrainingResults: "model results!"})
+        // Define a model for linear regression.
+        const model = tf.sequential();
+        model.add(tf.layers.dense({ units: 1, inputShape: [1] }));
+
+        model.compile({ loss: 'meanSquaredError', optimizer: 'sgd' });
+
+        // Generate some synthetic data for training.
+        const xs = tf.tensor2d([1, 2, 3, 4], [4, 1]);
+        const ys = tf.tensor2d([1, 3, 5, 7], [4, 1]);
+
+        // Train the model using the data.
+        model.fit(xs, ys, { epochs: 100 }).then(() => {
+            // Use the model to do inference on a data point the model hasn't seen before:
+            this.setState({ modelTrainingResults: model.predict(tf.tensor2d([5], [1, 1])).toString() }) ;
+        });
     }
 
     render() {
