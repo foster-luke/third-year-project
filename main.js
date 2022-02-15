1b7184d7ff6f0d1ad4c7cac8e060365c08fc22b7
Luke Foster <foster.luke@hotmail.com>
2022-02-15 17:05:04 +0000

    Add bootstrap

--- main.js
+++ main.js
@@ -6,8 +6,8 @@ const tf = require('@tensorflow/tfjs-node')
 function createWindow() {
     // Create the browser window.
     const mainWindow = new BrowserWindow({
-        width: 800,
-        height: 600,
+        width: 1400,
+        height: 1000,
         webPreferences: {
             preload: path.join(__dirname, 'preload.js')
         }
@@ -17,7 +17,7 @@ function createWindow() {
     mainWindow.loadFile('index.html')
 
     // Open the DevTools.
-    // mainWindow.webContents.openDevTools()
+    mainWindow.webContents.openDevTools()
 }
 
 // This method will be called when Electron has finished
