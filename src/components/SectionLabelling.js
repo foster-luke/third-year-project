d6be77c60bce9e1734394d3ea0175cf08df2672f
Luke Foster <foster.luke@hotmail.com>
2022-03-04 17:26:24 +0000

    Add section labelling form

--- src/components/SectionLabelling.js
+++ src/components/SectionLabelling.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 
 /**
  * Section labelling component
@@ -35,7 +35,69 @@ class SectionLabelling extends React.Component {
    */
   render() {
     return <div id="sectionLabelling" className='container p-3'>
-      Section Labelling
+      <form onSubmit={this.handlePodcastFormSubmit}>
+        <div className="mb-4 row">
+          <div className="col-sm-6">
+            <legend>Labelled Sections</legend>
+          </div>
+          <div className="col-sm-6">
+            Podcast example 1<br />
+            Episode 123
+          </div>
+        </div>
+        <div className="mb-1 row">
+          <div className="col-sm-4">
+            Section 1
+          </div>
+          <div className="col-sm-4">
+            <input
+              type='text'
+              id='section1StartTime'
+              placeholder='Start Time'
+              className='form-control form-control-sm'
+              // onChange={this.handleInputChange}
+            />
+          </div>
+          <div className="col-sm-4">
+            <input
+              type='text'
+              id='section1EndTime'
+              placeholder='End Time'
+              className='form-control form-control-sm'
+              // onChange={this.handleInputChange}
+            />
+          </div>
+        </div>
+        <div className="mb-2 row mt-4">
+          <div className="col">
+            <button className='btn btnGrey'>
+              Add New Section
+            </button>
+          </div>
+        </div>
+        <div className="mb-1 row">
+          <div className="col-sm-6">
+            <button
+              type="submit"
+              className="btn btnGrey"
+              id="saveLabelledSections"
+            >
+            Save Labelled Sections
+            </button>
+          </div>
+          <div className="col-sm-6">
+            {/* <div
+              className={'alert alert-danger py-0 ' +
+                (this.state.podcastSlugAlert ? '' : 'invisible')
+              }
+              id="podcastSelectAlert"
+              role="alert"
+            >
+              This name is already in use
+            </div> */}
+          </div>
+        </div>
+      </form>
     </div>;
   }
 }
